8676bff84d29105e36f6e8d0e7b860854ea7312c
Ruud0511 <post@ruud.co>
2023-09-22 15:28:08 +0200

    project finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,24 @@
 import React from 'react';
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
 import Whitening from './pages/whitening/Whitening.jsx';
 import Home from './pages/home/Home.jsx';
 import Appointments from './pages/appointments/Appointments.jsx';
 import Cavities from './pages/cavities/Cavities.jsx';
 import Navigation from './components/navigation/./Navigation.jsx';
 
+
 function App() {
     return (
         <>
             <Navigation />
-            <Home />
-            <Whitening />
-            <Appointments />
-            <Cavities />
+            <Routes>
+                <Route path="/" element={<Home/>}/>
+                <Route path="/whitening" element={<Whitening/>}/>
+                <Route path="/appointments" element={<Appointments/>}/>
+                <Route path="/cavities" element={<Cavities/>}/>
+                {/*<Route path="*" element={<NotFound/>}/>*/}
+            </Routes>
         </>
     );
 }
